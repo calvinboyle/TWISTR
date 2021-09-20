d1c38e396b1254d618d3ac2af835221623aadc1c
calvinboyle <60908342+calvinboyle@users.noreply.github.com>
2021-09-20 17:22:04 -0400

    Add files via upload

--- code/TWISTR_application.m
+++ code/TWISTR_application.m
@@ -118,9 +118,13 @@ global test_status
 test_status = true;
 
 global torque_data
+global torque_raw_data
 torque_data = [];
+torque_raw_data = [];
+
 global pos_data
 pos_data = [];
+
 global tension_data
 tension_data = [];
 
@@ -134,11 +138,18 @@ while test_status
     group.send(cmd);
     
     fbk = group.getNextFeedback();
-    torque_data = [torque_data (-1*(-13.98*fbk.effort + 3.2349))];
-    pos_data = [pos_data rad2deg(fbk.position-start_pos)];
+    tor=fbk.effort; 
+    pos=fbk.position-start_pos;
+    
+    torque_data = [torque_data (-1*(-13.98*tor + 3.2349))];
+    torque_raw_data = [torque_raw_data tor];
+    
+    pos_data = [pos_data rad2deg(pos)];
+    
     tension_data = [tension_data (1891.6*readVoltage(a, 'A0') + 35.42)];
-    plot(handles.chartLeft, pos_data, torque_data, 'o');
-    plot(handles.chartRight, torque_data, tension_data, 'o');
+        
+    plot(handles.chartLeft, pos_data, torque_data, 'r-');
+    plot(handles.chartRight, torque_data, tension_data, 'r-');
     
     drawnow;
 end
@@ -151,31 +162,6 @@ function buttonEndTest_Callback(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 global test_status
 test_status = false;
-
-
-
-function boxFilename_Callback(hObject, eventdata, handles)
-% hObject    handle to boxFilename (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: get(hObject,'String') returns contents of boxFilename as text
-%        str2double(get(hObject,'String')) returns contents of boxFilename as a double
-
-
-
-% --- Executes during object creation, after setting all properties.
-function boxFilename_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to boxFilename (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: edit controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
-
 
 
 % --- Executes on button press in buttonMotorStop.
@@ -250,6 +236,42 @@ function buttonSaveData_Callback(hObject, eventdata, handles)
 % hObject    handle to buttonSaveData (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+
+[file, path] = uiputfile;
+
+global pos_data
+global torque_data
+global torque_raw_data
+global tension_data
+
+data = [pos_data.' torque_data.' torque_raw_data.' tension_data.'];
+T = array2table(data);
+T.Properties.VariableNames(1:4) = {'Angular Position (deg)', 'Calibrated Effort (inlbs)', 'Raw Effort (??)', 'Preload (lbf)'};
+writetable(T, string(path) + string(file) + ".csv");
+
+figure
+sm1=smooth(torque_data);
+plot(pos_data, sm1, 'r-');
+grid on
+grid minor
+set(gcf, 'position', [10, 10, 600, 800]);
+title(string(file) + ' - Angle vs Torque');
+xlabel('Angular Position (degrees)');
+ylabel('Torque (inlbs)');
+saveas(gcf, string(path) + string(file) + "_AvsT.png");
+
+figure
+sm2=smooth(tension_data);
+plot(sm1, sm2, 'r-');
+grid on
+grid minor
+set(gcf, 'position', [10, 10, 600, 800]);
+title(string(file) + ' - Torque vs Preload');
+xlabel('Torque (inlbs)');
+ylabel('Preload (lbf)');
+saveas(gcf, string(path) + string(file) + "_TvsP.png");
+
+
 
 
 % --- Executes on selection change in popLoadCellSize.
